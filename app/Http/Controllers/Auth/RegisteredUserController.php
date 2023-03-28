bb35633c6ce2242b641da2618da2b25511f0cbe9
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-03-28 11:55:01 +0200

    Zmeneny typ hashu na ARgon2ID

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,7 +39,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => password_hash($request->password, PASSWORD_ARGON2ID, ['memory_cost' => 2048, 'time_cost' => 4, 'threads' => 3]),
         ]);
 
         event(new Registered($user));
