1e402357827be3aaf4af1afe29515794abea42ed
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-03-31 16:51:43 +0200

    Upravena validacia na unikatnu komb pri prihlaske

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -15,10 +16,11 @@ class ApplicationController extends Controller
 
     public function store()
     {
+        
         $attributes = request()->validate([
             'name' => ['required', 'max:255'],
             'lastname' => ['required', 'max:255'],
-            'email' => ['required', 'email', 'max:255', Rule::unique('applications', 'email')],
+            'email' => 'required|email|max:255|unique:applications,email,NULL,id,email,'.request()->email.',academy_id,'.request()->academy_id.',coursetype_id,'.request()->coursetype_id,
             'academy_id' => ['required', 'integer',Rule::exists('academies', 'id')],
             'coursetype_id' => ['required', 'integer',Rule::exists('course_types', 'id')],
             'days' => ['required', 'integer'],
--- database/migrations/2023_03_30_211919_create_applications_table.php
+++ database/migrations/2023_03_30_211919_create_applications_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('lastname');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->foreignId('academy_id')->constrained();
             $table->foreignId('coursetype_id')->constrained('course_types');
             $table->integer('days');
