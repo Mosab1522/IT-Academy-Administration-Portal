a1ca3c4e19d55a93fa5581f09789b01af61f74c3
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-04-05 21:20:58 +0200

    Upravene na layout pre include tailwindu

--- resources/views/admin/create-coursetype.blade.php
+++ resources/views/admin/create-coursetype.blade.php
@@ -1,11 +1,4 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-
+<x-layout/>
 <x-setting heading="Vytvoriť akadémiu">
     <form action="/admin/coursetype" method="post">
         @csrf
