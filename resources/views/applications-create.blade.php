f99534285360bc2e78795d4daab3f75536542ace
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-04-09 23:20:42 +0200

    vytvaranie studentom finish

--- resources/views/applications-create.blade.php
+++ resources/views/applications-create.blade.php
@@ -25,7 +25,7 @@
                         @endphp
                         @foreach (\App\Models\Academy::with(['coursetypes','applications'])
                         ->get() as $academ)
-                        <option value="{{ $academ->id }}" data-id="{{ $academ->id }}" data-option="-1">{{
+                        <option value="{{ $academ->id }}" data-id="{{ $academ->id }}" data-option="-1" {{old('academy_id') == $academ->id ? 'selected' : ''}}>{{
                             ucwords($academ->name)}}</option>
                         @endforeach
                         {{-- <option value="" disabled selected hidden>Akadémia</option>
@@ -49,46 +49,48 @@
                         $academy = \App\Models\CourseType::all();
                         @endphp --}}
                         @foreach (\App\Models\CourseType::with(['academy','applications'])->get() as $type)
-                        <option value="{{ $type->id }}" data-id="{{ $type->id }}" data-option="{{ $type->academy_id }}">{{
+                        <option value="{{ $type->id }}" data-id="{{ $type->id }}" data-option="{{ $type->academy_id }}" {{old('coursetype_id') == $type->id ? 'selected' : ''}}>{{
                             ucwords($type->name) }}</option>
                         @endforeach
                     </select>
                 </div>
             </div>
 
-            <x-form.field>
+           
 
-                <x-form.label name="dni výučby" />
-
-                <select name="days" id="days">
-                    <option value="" disabled selected hidden>Dni výučby</option>
-                    <option value="1">Týždeň</option>
-                    <option value="2">Víkend</option>
-                    <option value="3">Nezáleží</option>
-                    {{-- <option value="1" data-id="1" data-option="2">Týždeň</option>
-                    <option value="1" data-id="1" data-option="3">Týždeň</option>
-                    <option value="2" data-id="2" data-option="3">Víkend</option>
-                    <option value="3" data-id="3" data-option="3">Nezáleží</option>
-                    <option value="1" data-id="1" data-option="4">Týždeň</option> --}}
-                </select>
-            </x-form.field>
-
-            <x-form.field>
-
-                <x-form.label name="čas výučby" />
-
-                <select name="time" id="time">
-                    <option value="" disabled selected hidden>Čas výučby</option>
-                    <option value="1">Ranný</option>
-                    <option value="2">Poobedný</option>
-                    <option value="3">Nezáleží</option>
-                    {{-- <option value="1" data-id="1" data-option="2">Ranný</option>
-                    <option value="4" data-id="1" data-option="3">Ranný (Týždeň/Víkend)</option>
-                    <option value="5" data-id="2" data-option="3">Poobedný (Týždeň)</option>
-                    <option value="3" data-id="3" data-option="3">Nezáleží</option>
-                    <option value="1" data-id="1" data-option="4">Ranný</option> --}}
-                </select>
-            </x-form.field>
+                <x-form.field>
+                    <div class="flex">
+                        <div>
+                            <x-form.label name="dni výučby" />
+                            <select name="days" id="days">
+                                <option value="" disabled selected hidden>Dni výučby</option>
+                                <option value="1" {{old('days') == 1 ? 'selected' : ''}}>Týždeň</option>
+                                <option value="2" {{old('days') == 2 ? 'selected' : ''}}>Víkend</option>
+                                <option value="3" {{old('days') == 3 ? 'selected' : ''}}>Nezáleží</option>
+                                {{-- <option value="1" data-id="1" data-option="2">Týždeň</option>
+                                <option value="1" data-id="1" data-option="3">Týždeň</option>
+                                <option value="2" data-id="2" data-option="3">Víkend</option>
+                                <option value="3" data-id="3" data-option="3">Nezáleží</option>
+                                <option value="1" data-id="1" data-option="4">Týždeň</option> --}}
+                            </select>
+                        </div>
+                        <div class="ml-4">
+                            <x-form.label name="čas výučby" />
+                            <select name="time" id="time">
+                                <option value="" disabled selected hidden>Čas výučby</option>
+                                <option value="1" {{old('time') == 1 ? 'selected' : ''}}>Ranný</option>
+                                <option value="2" {{old('time') == 3 ? 'selected' : ''}}>Poobedný</option>
+                                <option value="3" {{old('time') == 3 ? 'selected' : ''}}>Nezáleží</option>
+                                {{-- <option value="1" data-id="1" data-option="2">Ranný</option>
+                                <option value="4" data-id="1" data-option="3">Ranný (Týždeň/Víkend)</option>
+                                <option value="5" data-id="2" data-option="3">Poobedný (Týždeň)</option>
+                                <option value="3" data-id="3" data-option="3">Nezáleží</option>
+                                <option value="1" data-id="1" data-option="4">Ranný</option> --}}
+                            </select>
+                        </div>
+                    </div>
+                </x-form.field>
+            
 
             {{-- <select name="category_id" id="category_id">
                 @php
