2d2fc0099c7e667fb10e3745d0538b3908c8e931
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-04-09 19:42:58 +0200

    zmena bocneho

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -4,34 +4,55 @@
         {{$heading}}
     </h1>
     <div class="flex">
-         <aside class="w-48 flex-shrink-0">
-            <h4 class="font-semibold mb-4"><a href="/" class="{{request()->is('/') ? 'text-blue-500' : ''}}">Links</a></h4>
+        <aside class="w-48 flex-shrink-0">
+            <h4 class="font-semibold mb-4"><a href="/" class="{{request()->is('/') ? 'text-blue-500' : ''}}">Nová
+                    prihláška</a></h4>
+
+            <h4 class="font-semibold mb-4"><a href="/admin/dashboard"
+                class="{{request()->is('admin/dashboard') ? 'text-blue-500' : ''}}">Prehľad</a></h4>
             <ul>
+                
                 <li>
-                    <a href="/admin/dashboard" class="{{request()->is('admin/dashboard') ? 'text-blue-500' : ''}}">Dashboard</a>
+                    <a href="/admin/academies"
+                        class="{{request()->is('admin/academies')||request()->is('admin/coursetypes')||request()->is('admin/students')||request()->is('admin/applications') ? 'font-bold' : ''}}">Spravovanie</a>
+                    <ul class="ml-4 text-sm">
+                        <li>
+                            <a href="/admin/academies"
+                                class="{{request()->is('admin/academies') ? 'text-blue-500' : ''}}">- Akadémie</a>
+                        </li>
+                        <li>
+                            <a href="/admin/coursetypes"
+                                class="{{request()->is('admin/coursetypes') ? 'text-blue-500' : ''}}">- Typy kurzov</a>
+                        </li>
+                        <li>
+                            <a href="/admin/students"
+                                class="{{request()->is('admin/students') ? 'text-blue-500' : ''}}">- Študenti</a>
+                        </li>
+                        <li>
+                            <a href="/admin/applications"
+                                class="{{request()->is('admin/applications') ? 'text-blue-500' : ''}}">- Prihlášky</a>
+                        </li>
+                    </ul>
+                </li>
+                <ul>
+                    <li>
+                        <a href="/admin/academies/create"
+                            class="{{request()->is('admin/*/create') ? 'font-bold' : ''}}">Vytvorenie</a>
                         <ul class="ml-4 text-sm">
                             <li>
-                                <a href="/admin/academies" class="{{request()->is('admin/academies') ? 'text-blue-500' : ''}}">- Akadémie</a>
+                                <a href="/admin/academies/create"
+                                    class="{{request()->is('admin/academies/create') ? 'text-blue-500' : ''}}">- Nová
+                                    akadémia</a>
                             </li>
                             <li>
-                                <a href="/admin/coursetypes" class="{{request()->is('admin/coursetypes') ? 'text-blue-500' : ''}}">- Typy kurzov</a>
-                            </li>
-                            <li>
-                                <a href="/admin/students" class="{{request()->is('admin/students') ? 'text-blue-500' : ''}}">- Študenti</a>
-                            </li>
-                            <li>
-                                <a href="/admin/applications" class="{{request()->is('admin/applications') ? 'text-blue-500' : ''}}">- Prihlášky</a>
+                                <a href="/admin/coursetypes/create"
+                                    class="{{request()->is('admin/coursetypes/create') ? 'text-blue-500' : ''}}">- Nový
+                                    kurz</a>
                             </li>
                         </ul>
-                </li>
-                <li>
-                    <a href="/admin/academies/create" class="{{request()->is('admin/academies/create') ? 'text-blue-500' : ''}}">New Academy</a>
-                </li>
-                <li>
-                    <a href="/admin/coursetypes/create" class="{{request()->is('admin/coursetypes/create') ? 'text-blue-500' : ''}}">New Course Type</a>
-                </li>
-            </ul>
-        </aside> 
+                    </li>
+                </ul>
+        </aside>
         <main class="flex-1">
             <x-panel>
                 {{$slot}}
