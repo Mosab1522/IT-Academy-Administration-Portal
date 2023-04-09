23f9cc088c17f7958f2cd0b82ae008c2892cc31e
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2023-04-09 23:12:55 +0200

    Opravene chyby vo vytvarani prihlasky studentom

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -9,6 +9,7 @@ use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Validator;
 
 class ApplicationController extends Controller
@@ -50,21 +51,23 @@ class ApplicationController extends Controller
 
     public function store()
     {
+
         $email['email'] = request()->email;
         $rule = array('email' => ['required', 'email', 'max:255', Rule::unique('students', 'email')]);
         $validation = Validator($email, $rule);
 
 
         if ($validation->fails()) {
+            
             $rule1 = Rule::exists('students', 'name')->where('email', request()->email);
             $rule2 = Rule::exists('students', 'lastname')->where('email', request()->email);
 
             $student = Student::firstWhere('email', $email['email']);
-            $rule = array('id' => 'unique:applications,id,NULL,id,id,' . $student['id'] . ',academy_id,' . request()->academy_id . ',coursetype_id,' . request()->coursetype_id);
-            $value['id'] = $student['id'];
+            $rule = array('student_id' => 'unique:applications,student_id,NULL,id,student_id,' . $student['id'] . ',academy_id,' . request()->academy_id . ',coursetype_id,' . request()->coursetype_id);
+            $value['student_id'] = $student['id'];
             $validation = Validator($value, $rule);
             if ($validation->fails()) {
-                dd($validation);
+                throw ValidationException::withMessages(['email' => 'Takáto prihláška už existuje']);
             } else {
                 $attributes = request()->validate([
                     'name' => ['required', 'max:255', $rule1],
--- resources/views/applications-create.blade.php
+++ resources/views/applications-create.blade.php
@@ -1,10 +1,4 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
+<x-layout/>
 
 <x-setting heading="Prihlásiť sa na kurz">
     <form action="/" method="post">
@@ -19,47 +13,48 @@
         <x-form.textarea name="body" /> --}}
 
         <x-form.field>
-
-            <x-form.label name="akadémia" />
-
-            <!-- parent -->
-            <select name="academy_id" class="combo-a" data-nextcombo=".combo-b">
-                <option value="" disabled selected hidden>Akadémia</option>
-                @php
-                $academy = \App\Models\Academy::all();
-                @endphp
-                @foreach (\App\Models\Academy::all() as $academ)
-                <option value="{{ $academ->id }}"  data-id="{{ $academ->id }}" data-option="-1">{{ ucwords($academ->name)}}</option>
-                @endforeach
-                {{-- <option value="" disabled selected hidden>Akadémia</option>
-                <option value="1" data-id="1" data-option="-1">Cisco</option>
-                <option value="2" data-id="2" data-option="-1">Adobe</option> --}}
-
-            </select>
-
-            <x-form.field>
-                <x-form.label name="typ kurzu" />
-                <!-- child -->
-                {{-- <select name="coursetype_id" id="coursetype_id" class="combo-b" data-nextcombo=".combo-c" disabled>
-                    <option value="" disabled selected hidden>Typ kurzu</option>
-                    <option value="1" data-id="1" data-option="1">Lahky</option>
-                    <option value="2" data-id="2" data-option="1">Stredny</option>
-                    <option value="3" data-id="3" data-option="2">Photoshop</option>
-                    <option value="4" data-id="4" data-option="2">Illustrator</option>
-                </select> --}}
-            </x-form.field>
-
-            <select name="coursetype_id" id="coursetype_id" class="combo-b" disabled>
-                <option value="" disabled selected hidden>Typ kurzu</option>
-                {{-- @php
-                $academy = \App\Models\CourseType::all();
-                @endphp --}}
-                @foreach (\App\Models\CourseType::all() as $type)
-                <option value="{{ $type->id }}" data-id="{{ $type->id }}" data-option="{{ $type->academy_id }}">{{ ucwords($type->name) }}</option>
-    
-                @endforeach
-
-            </select>
+            <div class="flex">
+                <div>
+                    <x-form.label name="akadémia" />
+                    <!-- parent -->
+                    <select name="academy_id" class="combo-a" data-nextcombo=".combo-b">
+                        <option value="" disabled selected hidden>Akadémia</option>
+                        @php
+                        $academy = \App\Models\Academy::with(['coursetypes','applications'])
+                                ->get();
+                        @endphp
+                        @foreach (\App\Models\Academy::with(['coursetypes','applications'])
+                        ->get() as $academ)
+                        <option value="{{ $academ->id }}" data-id="{{ $academ->id }}" data-option="-1">{{
+                            ucwords($academ->name)}}</option>
+                        @endforeach
+                        {{-- <option value="" disabled selected hidden>Akadémia</option>
+                        <option value="1" data-id="1" data-option="-1">Cisco</option>
+                        <option value="2" data-id="2" data-option="-1">Adobe</option> --}}
+                    </select>
+                </div>
+                <div class="ml-4">
+                    <x-form.label name="typ kurzu" />
+                    <!-- child -->
+                    {{-- <select name="coursetype_id" id="coursetype_id" class="combo-b" data-nextcombo=".combo-c" disabled>
+                        <option value="" disabled selected hidden>Typ kurzu</option>
+                        <option value="1" data-id="1" data-option="1">Lahky</option>
+                        <option value="2" data-id="2" data-option="1">Stredny</option>
+                        <option value="3" data-id="3" data-option="2">Photoshop</option>
+                        <option value="4" data-id="4" data-option="2">Illustrator</option>
+                    </select> --}}
+                    <select name="coursetype_id" id="coursetype_id" class="combo-b" disabled>
+                        <option value="" disabled selected hidden>Typ kurzu</option>
+                        {{-- @php
+                        $academy = \App\Models\CourseType::all();
+                        @endphp --}}
+                        @foreach (\App\Models\CourseType::with(['academy','applications'])->get() as $type)
+                        <option value="{{ $type->id }}" data-id="{{ $type->id }}" data-option="{{ $type->academy_id }}">{{
+                            ucwords($type->name) }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
 
             <x-form.field>
 
