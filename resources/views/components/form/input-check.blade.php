b8eb81f7f24ef4357bbbf9c3c3382a4a52c8f1d3
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2024-04-23 23:11:20 +0200

    komponenty

---
+++ resources/views/components/form/input-check.blade.php
@@ -0,0 +1,6 @@
+@props(['name', 'title'])
+<div class="flex items-center mt-6">
+    <input id="{{$name}}" name="{{$name}}" type="checkbox"
+        class="w-6 h-6 lg:w-4 lg:h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" />
+    <label for="{{$name}}" class="ml-2 block text-gray-700 text-sm leading-5.6">{{$title}}</label>
+</div>
---
+++ resources/views/components/table/td.blade.php
@@ -0,0 +1,5 @@
+@props(['url'])
+    <span class="inline lg:hidden">{{$slot}}</span>
+    <a class=" hidden lg:inline hover:underline underline-offset-2" href="/admin/{{$url}}">
+    {{$slot}}</a>
+   
