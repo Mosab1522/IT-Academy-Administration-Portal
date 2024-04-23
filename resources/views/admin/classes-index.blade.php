c3fd622d83ff0b53342c15a6e7a8b4027af5f3a9
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2024-04-23 23:11:49 +0200

    triedy  responzivita a komponenty

--- resources/views/admin/classes-index.blade.php
+++ resources/views/admin/classes-index.blade.php
@@ -7,28 +7,28 @@
                 <h3 class="text-lg font-semibold text-gray-900 mb-4">Vytvorenie triedy</h3>
                 <form action="/admin/classes/create" method="post">
                     @csrf
-                    <div class="flex items-center mt-6">
-                        <input id="students" name="students" type="checkbox"
-                            class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" />
-                        <label for="students" class="ml-2 block text-gray-700 text-sm leading-5.6">Zapísať všetkých študentov ktorí majú
-                            prihlášku na kurz</label>
-                    </div>
+                    <x-form.input-check name="students" title="Zapísať všetkých študentov ktorí majú
+                    prihlášku na kurz"/>
                     <x-form.field>
                         <x-form.input name="name" type="text" title="Názov" placeholder="Názov" />
                     </x-form.field>
                     <x-form.field>
 
-                        <div class="items-center mt-6">
+                        <div class="items-center mt-6 ">
                             <x-form.label name="type" title="Typ kurzu" />
-
+    
                             <div class="flex items-center mt-1">
                                 <x-form.input-radio name="type" for="type_student" value="0">
                                     Študentský
                                 </x-form.input-radio>
+    
                                 <x-form.input-radio class="ml-6" name="type" for="type_instructor" value="1">
                                     Inštruktorský
                                 </x-form.input-radio>
+    
+    
                             </div>
+    
                         </div>
                         {{-- <div class="items-center">
                             <x-form.label name="typ kurzu:" />
@@ -183,60 +183,50 @@
 
                     </x-form.field>
 
-                    <x-form.button class="mt-6">
+                    <x-form.button class="mt-6 md:w-auto w-full sm:w-auto">
                         Odoslať
                     </x-form.button>
                 </form>
             </div>
         </div>
     </x-slot:create>
-    <div class="bg-white p-6 rounded-lg shadow mb-4 flex justify-between items-end">
-        <form method="get" action="{{ route('admin.classes.index') }}" class="flex flex-wrap items-end">
-            @csrf
+    <x-form.search action="{{ route('admin.coursetypes.index') }}" text="Filtrovať a zoradiť">
+        @csrf
 
-            @if(request()->filled('search'))
-            <input type="hidden" name="search" value="{{request()->input('search')}}" />
-            @endif
-            <x-form.search-select name="orderBy" title="Zoradiť podľa">
-                <option value="created_at" {{request()->input('orderBy')=='created_at' ? 'selected' : ''}}>Dátumu
-                    vytvorenia</option>
-                <option value="updated_at" {{request()->input('orderBy')=='updated_at' ? 'selected' : ''}}>Dátumu
-                    poslednej úpravy</option>
-            </x-form.search-select>
-            <x-form.search-select name="orderDirection" title="Smer zoradenia">
-                <option value="desc" {{request()->input('orderDirection')=='desc' ? 'selected' : ''}}>Od najnovšej
-                </option>
-                <option value="asc" {{request()->input('orderDirection')=='asc' ? 'selected' : ''}}>Od najstaršej
-                </option>
-            </x-form.search-select>
-            {{-- <div class="form-group">
-                <select class="form-control" id="filterBy" name="filterBy[]" multiple>
-                    <option value="academy_id|1">Akadémia 1</option>
-                    <option value="academy_id|2">Akadémia 2</option>
-                    <option value="coursetype_id|1">Typ kurzu 1</option>
-                    <option value="coursetype_id|2">Typ kurzu 2</option>
-                </select>
-            </div> --}}
-            <x-form.search-select name="academy_id" title="Akadémia">
-                <option value="" disabled selected hidden>Akadémia</option>
-                @php
-                $academy = \App\Models\Academy::with(['coursetypes','applications'])
-                ->get();
-                $coursetype = \App\Models\CourseType::with(['academy','applications'])->get();
-                @endphp
+        @if(request()->filled('search'))
+        <input type="hidden" name="search" value="{{request()->input('search')}}" />
+        @endif
+        <x-form.search-select name="orderBy" title="Zoradiť podľa">
+            <option value="created_at" {{request()->input('orderBy')=='created_at' ? 'selected' : ''}}>Dátumu
+                vytvorenia</option>
+            <option value="updated_at" {{request()->input('orderBy')=='updated_at' ? 'selected' : ''}}>Dátumu
+                poslednej úpravy</option>
+        </x-form.search-select>
+        <x-form.search-select name="orderDirection" title="Smer zoradenia">
+            <option value="desc" {{request()->input('orderDirection')=='desc' ? 'selected' : ''}}>Od najnovšej
+            </option>
+            <option value="asc" {{request()->input('orderDirection')=='asc' ? 'selected' : ''}}>Od najstaršej
+            </option>
+        </x-form.search-select>
+        <x-form.search-select name="academy_id" title="Akadémia">
+            <option value="" disabled selected hidden>Akadémia</option>
+            @php
+            $academy = \App\Models\Academy::with(['coursetypes','applications'])
+            ->get();
+            $coursetype = \App\Models\CourseType::with(['academy','applications'])->get();
+            @endphp
 
-                <option value="" data-option="-1">Všetky</option>
+            <option value="" data-option="-1">Všetky</option>
 
-                @foreach ($academy as $academ)
-                <option value="{{ $academ->id }}" data-id="{{ $academ->id }}" data-option="-1" {{request()->
-                    input('academy_id')==$academ->id ? 'selected' : ''}}>{{
-                    ucwords($academ->name) }}</option>
-                @endforeach
-                </select>
-            </x-form.search-select>
-            <x-form.button class="ml-2">Filtrovať a zoradiť</x-form.button>
-        </form>
-        <form method="get" action="{{ route('admin.classes.index') }}" class="flex flex-wrap items-end">
+            @foreach ($academy as $academ)
+            <option value="{{ $academ->id }}" data-id="{{ $academ->id }}" data-option="-1" {{request()->
+                input('academy_id')==$academ->id ? 'selected' : ''}}>{{
+                ucwords($academ->name) }}</option>
+            @endforeach
+
+        </x-form.search-select>
+
+        <x-slot:search>
             @csrf
             @if(request()->filled('orderBy'))
             <input type="hidden" name="orderBy" value="{{request()->input('orderBy')}}" />
@@ -248,65 +238,41 @@
             @elseif(request()->filled('academy_id'))
             <input type="hidden" name="academy_id" value="{{request()->input('academy_id')}}" />
             @endif
-            <div class="w-full md:w-auto md:mr-4 mt-4 md:mt-0">
-
-                <input type="text" name="search" value="{{old('search')}}"
-                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-sm"
-                    placeholder="Vyhľadávanie">
-            </div>
-            <x-form.button class="ml-2">Hľadať</x-form.button>
-        </form>
-    </div>
-    <div class="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
-        <div class="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8 ">
-            <div class="overflow-x-auto relative rounded-lg shadow">
-                <table class="w-full text-sm text-left text-gray-800 dark:text-gray-800 shadow-md">
-                    <thead class="text-xs uppercase bg-gray-200">
-                        <tr>
+        </x-slot:search>
+    </x-form.search>
+    <x-single-table>
+        <x-slot:head>
+                        
                             <th scope="col" class="py-3 px-6">Názov triedy</th>
                             <th scope="col" class="py-3 px-6">Kurz</th>
                             <th scope="col" class="py-3 px-6">Inštruktor</th>
                             <th scope="col" class="py-3 px-6">Dni / čas</th>
                             <th scope="col" class="py-3 px-6">Počet študentov</th>
                             <th scope="col" class="py-3 px-6 w-40">Akcie</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                        </x-slot:head>
                         @foreach ($classes as $class)
                         <tr
                             class="bg-white border-b dark:bg-white dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-50">
-                            <td class="py-4 px-6">{{$class->name}}</td>
+                            <td class="py-4 px-6"><x-table.td url="classes/{{ $class->id }}">{{$class->name}}</x-table.td></td>
                             <td class="py-4 px-6">
+                                <x-table.td url="coursetypes/{{ $class->coursetype->id }}">
                                 {{$class->coursetype->name}} - {{$class->coursetype->type==0 ? 'študentský' :
                                 'inštruktorský'}} ({{$class->academy->name}} akadémia)
+                                </x-table.td>
                             </td>
-                            <td class="py-4 px-6">{{$class->instructor->name}} {{$class->instructor->lastname}}</td>
+                            <td class="py-4 px-6"><x-table.td url="instructors/{{ $class->instructor->id }}">
+                                {{$class->instructor->name}} {{$class->instructor->lastname}}
+                                </x-table.td></td>
                             <td class="py-4 px-6">
                                 {{$class->days== 1 ? 'Týždeň' : ''}} {{$class->days== 2 ? 'Víkend' : ''}}
                                 {{$class->days== 3 ? 'Nezáleží' : ''}} / {{$class->time== 1 ? 'Ranný' : ''}}
                                 {{$class->time== 2 ? 'Poobedný' : ''}} {{$class->time== 3 ? 'Nezáleží' : ''}}
                             </td>
                             <td class="py-4 px-6">{{$class->students->count()}}</td>
-                            <td class="py-4 px-6 text-right">
-                                <a href="/admin/classes/{{ $class->id }}"
-                                    class="text-blue-600 hover:text-blue-700 hover:underline ">Upraviť</a>
-                                &nbsp;
-                                <form method="POST" action="/admin/classes/{{ $class->id }}" class="inline">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                        class="text-red-600 hover:text-red-700 hover:underline ">Vymazať</button>
-                                </form>
-                            </td>
+                            <x-table.td-last url="classes/{{ $class->id }}" delete=1 itemName="triedu {{$class->name}}" />
+                            
                         </tr>
                         @endforeach
-                    </tbody>
-                </table>
-            </div>
-
-
-
-        </div>
-    </div>
+                    </x-single-table>
 
 </x-setting>
