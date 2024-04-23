5413d4270d281c878091f1bf4d7559fd2907e952
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2024-04-23 20:58:34 +0200

    uprava tabulky

---
+++ resources/views/components/single-table.blade.php
@@ -0,0 +1,13 @@
+<div class="overflow-x-auto relative rounded-lg shadow mt-6 align-middle ">
+    <table class="w-full text-sm text-left text-gray-800 dark:text-gray-800 ">
+        <thead class="text-xs uppercase bg-gray-200">
+                        <tr>
+                           {{$head}}
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        {{{$slot}}}
+                    </tbody>
+                </table>
+            </div>
+     
