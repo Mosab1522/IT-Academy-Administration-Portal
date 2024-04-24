2d5e6ce7eb9e3a3eacdf8751e45e7a0a91270f45
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2024-04-24 15:45:01 +0200

    responzivita

--- resources/views/admin/lessons-index.blade.php
+++ resources/views/admin/lessons-index.blade.php
@@ -11,7 +11,7 @@
                     <x-form.label name="Zapísať všetkých ktorý majú prihlášku na kurz?" />
                     <input type="checkbox" name="students"> --}}
                     <x-form.field>
-                    <x-form.input name="title" type="text" title="Názov hodiny" placeholder="Názov hodiny" />
+                        <x-form.input name="title" type="text" title="Názov hodiny" placeholder="Názov hodiny" />
                     </x-form.field>
                     <x-form.field>
 
@@ -63,7 +63,7 @@
 
                     </x-form.field>
 
-                    <x-form.button class="mt-6">
+                    <x-form.button class="mt-6 md:w-auto w-full sm:w-auto">
                         Odoslať
                     </x-form.button>
                 </form>
@@ -71,147 +71,114 @@
         </div>
     </x-slot:create>
 
-    <div class="bg-white p-6 rounded-lg shadow mb-4 flex justify-between items-end">
-        <form method="get" action="{{ route('admin.lessons.index') }}" class="flex flex-wrap items-end">
+    <x-form.search action="{{ route('admin.lessons.index') }}" text="Filtrovať a zoradiť">
             @csrf
             @if(request()->filled('search'))
             <input type="hidden" name="search" value="{{request()->input('search')}}" />
             @endif
             <x-form.search-select name="orderBy" title="Zoradiť podľa">
-                    <option value="created_at" {{request()->input('orderBy')=='created_at' ? 'selected' : ''}}>Dátumu
-                        vytvorenia</option>
-                    <option value="updated_at" {{request()->input('orderBy')=='updated_at' ? 'selected' : ''}}>Dátumu
-                        poslednej úpravy</option>
-        </x-form.search-select>
-        <x-form.search-select name="orderDirection" title="Smer zoradenia">
-            <option value="desc" {{request()->input('orderDirection')=='desc' ? 'selected' : ''}}>Od najnovšej
-            </option>
-            <option value="asc" {{request()->input('orderDirection')=='asc' ? 'selected' : ''}}>Od najstaršej
-            </option>
-</x-form.search-select>
+                <option value="created_at" {{request()->input('orderBy')=='created_at' ? 'selected' : ''}}>Dátumu
+                    vytvorenia</option>
+                <option value="updated_at" {{request()->input('orderBy')=='updated_at' ? 'selected' : ''}}>Dátumu
+                    poslednej úpravy</option>
+            </x-form.search-select>
+            <x-form.search-select name="orderDirection" title="Smer zoradenia">
+                <option value="desc" {{request()->input('orderDirection')=='desc' ? 'selected' : ''}}>Od najnovšej
+                </option>
+                <option value="asc" {{request()->input('orderDirection')=='asc' ? 'selected' : ''}}>Od najstaršej
+                </option>
+            </x-form.search-select>
 
-<x-form.search-select name="class_id" title="Trieda">
-    @php
-                    $classes = \App\Models\CourseClass::with(['instructor','students'])->get();
-                    @endphp
+            <x-form.search-select name="class_id" title="Trieda">
+                @php
+                $classes = \App\Models\CourseClass::with(['instructor','students'])->get();
+                @endphp
 
-                    <option value="" data-option="-1">Všetky</option>
+                <option value="" data-option="-1">Všetky</option>
 
-                    @foreach ($classes as $class)
-                    <option value="{{ $class->id }}" data-id="{{ $class->id }}" data-option="-1" {{request()->
-                        input('class_id')==$class->id ? 'selected' : ''}}>{{
-                        ucwords($class->name) }}</option>
-                    @endforeach
-</x-form.search-select>
+                @foreach ($classes as $class)
+                <option value="{{ $class->id }}" data-id="{{ $class->id }}" data-option="-1" {{request()->
+                    input('class_id')==$class->id ? 'selected' : ''}}>{{
+                    ucwords($class->name) }}</option>
+                @endforeach
+            </x-form.search-select>
 
 
 
-            <x-form.button class="ml-2">Filtrovať a zoradiť</x-form.button>
-        </form>
-
-        <!-- Search Form -->
-        <form method="get" action="{{ route('admin.lessons.index') }}" class="flex flex-wrap items-end">
+            <x-slot:search>
             @csrf
             @if(request()->filled('orderBy'))
-                <input type="hidden" name="orderBy" value="{{request()->input('orderBy')}}" />
-                <input type="hidden" name="orderDirection" value="{{request()->input('orderDirection')}}" />
-                @endif
-                @if(request()->filled('class_id'))
-                <input type="hidden" name="class_id" value="{{request()->input('class_id')}}" />
+            <input type="hidden" name="orderBy" value="{{request()->input('orderBy')}}" />
+            <input type="hidden" name="orderDirection" value="{{request()->input('orderDirection')}}" />
+            @endif
+            @if(request()->filled('class_id'))
+            <input type="hidden" name="class_id" value="{{request()->input('class_id')}}" />
 
-                @endif
-            <div class="w-full md:w-auto md:mr-4 mt-4 md:mt-0">
-
-                <input type="text" name="search" value="{{old('search')}}"
-                    class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-sm"
-                    placeholder="Vyhľadávanie">
-            </div>
-            <x-form.button class="ml-2">Hľadať</x-form.button>
-
-
-
-        </form>
-    </div>
+            @endif
+        </x-slot:search>
+    </x-form.search>
 
 
 
 
+    <div class="flex-auto mt-6">
 
-    <div class="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-6 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="overflow-x-auto relative rounded-lg shadow">
-                <div class="max-w-8xl mx-auto py-6 sm:px-6 lg:px-8">
-                    <!-- Iterate over classes -->
-                    @foreach ($lessons->groupBy('class.name') as $className => $lessonsInClass)
-                    @php
-                    // Assuming you can access the first lesson to get the class ID
-                    $firstLesson = $lessonsInClass->first();
-                    $classId = $firstLesson->class->id;
-                    @endphp
-                    <div class="mb-8">
-                        <h3 class=" leading-6  text-gray-700 mb-2">
-                            <a href="/admin/lessons/{{ $classId }}" class="hover:underline hover:text-gray-900">
-                            {{ $className }}
-                            </a>
-                        </h3>
-                        <div class="bg-white shadow overflow-hidden rounded-lg">
-                            <table class="w-full text-sm text-left text-gray-800 dark:text-gray-800 shadow-md">
-                                <thead class="text-xs uppercase bg-gray-200">
-                                    <tr>
-                                        <th scope="col" class="py-3 px-6">
-                                            Názov hodiny
-                                        </th>
-                                        <th scope="col" class="py-3 px-6">
-                                            Inštruktor
-                                        </th>
-                                        <th scope="col" class="py-3 px-6">
-                                            Dátum a trvanie
-                                        </th>
-                                        <th scope="col" class="py-3 px-8 w-40">
-                                            Akcie
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <!-- Iterate over lessons in this class -->
-                                    @foreach ($lessonsInClass as $lesson)
-                                    <tr class="bg-white border-b dark:bg-white dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-50">
-                                        <td class="py-4 px-6">
-                                            {{ $lesson->title }}
-                                        </td>
-                                        <td class="py-4 px-6">
-                                            {{ $lesson->instructor->name }} {{ $lesson->instructor->lastname }}
-                                        </td>
-                                        <td class="py-4 px-6">
-                                            {{ $lesson->lesson_date }} - {{ $lesson->duration }} minút
-                                        </td>
-                                        <td class="py-4 px-4 text-right">
-                                            <a href="/admin/lessons/{{ $lesson->id }}"
-                                                class="text-blue-600 hover:text-blue-700 hover:underline">Upraviť</a>
-                                            &nbsp;
-                                            <form method="POST" action="/admin/lessons/{{ $lesson->id }}"
-                                                class="inline">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit"
-                                                    class="text-red-600 hover:text-red-700 hover:underline">Vymazať</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    @endforeach
-                </div>
-
-            </div>
+        <!-- Iterate over classes -->
+        @foreach ($lessons->groupBy('class.name') as $className => $lessonsInClass)
+        @php
+        // Assuming you can access the first lesson to get the class ID
+        $firstLesson = $lessonsInClass->first();
+        $classId = $firstLesson->class->id;
+        @endphp
+        <div class=" mb-6">
+           
+                <h3 class="-mb-4 leading-6  text-gray-700">Hodiny triedy - 
+                    <a href="/admin/lessons/{{ $classId }}" class="hover:underline underline-offset-2 hover:text-gray-900">
+                        {{ $className }}
+                    </a>
+                </h3>
 
 
+                <x-single-table>
+                    <x-slot:head>
+                                <th scope="col" class="py-3 px-6">
+                                    Názov hodiny
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Inštruktor
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Dátum a trvanie
+                                </th>
+                                <th scope="col" class="px-3 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider lg:px-6 lg:py-3">
+                                    Akcie
+                                </th>
+                            </x-slot:head>
+                            <!-- Iterate over lessons in this class -->
+                            @foreach ($lessonsInClass as $lesson)
+                            <tr class="bg-white border-b dark:bg-white dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-50">
+                                <td class="py-4 px-6">
+                                    <x-table.td url="lessons/{{ $lesson->id }}">
+                                    {{ $lesson->title }}
+                                    </x-table.td>
+                                </td>
+                                <td class="py-4 px-6">
+                                    <x-table.td url="instructors/{{ $lesson->instructor->id }}">
+                                    {{ $lesson->instructor->name }} {{ $lesson->instructor->lastname }}
+                                    </x-table.td>
+                                </td>
+                                <td class="py-4 px-6">
+                                    {{ $lesson->lesson_date }} - {{ $lesson->duration }} minút
+                                </td>
+                                <x-table.td-last url="lessons/{{ $lesson->id }}" delete=1 itemName="hodinu {{$lesson->title}}" />
+                               
+                            </tr>
+                            @endforeach
+                        </x-single-table>
+        </div>
+            @endforeach
 
         </div>
-    </div>
 
 
 </x-setting>
