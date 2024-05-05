613b6333f571cbe84c98943401378e0ea469734f
Mosab1522 <92610140+Mosab1522@users.noreply.github.com>
2024-05-05 12:03:25 +0200

    zaciatok middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::unguard();
+
+        Gate::define('admin',function(User $user){
+          return $user->username == 'adminuser';
+        });
+  
+        Blade::if('admin',function(){
+            return request()->user()?->can('admin'); // nie dobre ak neni prihlaseny pouzivatel lebo tam bude null bez toho otaznika.. s ? je to optional
+        });
     }
 }
